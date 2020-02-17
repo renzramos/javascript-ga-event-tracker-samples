7d01b3020ed27501171bbd198cf990f876720a72
Renz Ramos <im.renzramos@gmail.com>
2020-02-17 10:18:05 +0800

    Create trackers.js
    initial commit

---
+++ trackers.js
@@ -0,0 +1,9 @@
+//Google Events Tracker
+
+ga('send', 'event', 'Premium Membership Buttons', 'Click', 'No Thanks');
+ga('send', 'event', 'Premium Membership Buttons', 'Click', 'Get Premium');
+ga('send', 'event', 'Premium Membership Buttons', 'Click', 'Close Premium');
+ga('send', 'event', 'Premium Membership Buttons', 'Click', 'Removed Premium');
+ga('send', 'event', 'Premium Membership Buttons', 'Click', 'Show Benefits');
+ga('send', 'event', 'Premium Membership Buttons', 'Click', 'Hide Benefits');
+
